92f7c0bc5dc0a29353b986a3d158aebc029e3a04
haoliu <hao.liu2017@nlpr.ia.ac.cn>
2019-12-04 17:38:14 -0800

    Modify

--- src/server.py
+++ src/server.py
@@ -7,25 +7,33 @@ import threading
 import time
 import random
 import xmlrpc.client
+import copy
+
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
+
 class threadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
     pass
+
 
 class TimeHandler():
     def __init__(self):
         self.election_lower = 1500
         self.election_higher = 3000
-        self.start = int(time.time()*1000)
+        self.start = int(time.time() * 1000)
         self.timeout = 0
+
     def timecount(self):
-        return int(time.time()*1000 - self.start)
+        return int(time.time() * 1000 - self.start)
+
     def reset(self):
-        self.start = int(time.time()*1000)
+        self.start = int(time.time() * 1000)
+
     def set_election_timeout(self):
-        self.timeout = random.randint(self.election_lower,self.election_higher)
+        self.timeout = random.randint(self.election_lower, self.election_higher)
+
     def set_heartbeat_timeout(self, interval):
         self.timeout = interval
 
@@ -36,6 +44,7 @@ def ping():
     print("Ping()")
     return True
 
+
 # Gets a block, given a specific hash value
 def getblock(h):
     """Gets a block"""
@@ -44,12 +53,14 @@ def getblock(h):
     blockData = bytes(4)
     return blockData
 
+
 # Puts a block
 def putblock(b):
     """Puts a block"""
     print("PutBlock()")
 
     return True
+
 
 # Given a list of hashes, return the subset that are on this server
 def hasblocks(hashlist):
@@ -58,21 +69,26 @@ def hasblocks(hashlist):
 
     return hashlist
 
+
 # Retrieves the server's FileInfoMap
 def getfileinfomap():
     """Gets the fileinfo map"""
     print("GetFileInfoMap()")
-
+    log.append([current_term, [1]])
     return fileinfomap
+
 
 # Update a file's fileinfo entry
 def updatefile(filename, version, hashlist):
     """Updates a file's fileinfo entry"""
-    print("UpdateFile("+filename+")")
+    if not isLeader():
+        raise ValueError("Not the leader")
+    else:
+        print("UpdateFile(" + filename + ")")
+        fileinfomap[filename] = [version, hashlist]
+        log.append([current_term, [2, filename, version, hashlist]])
+        return True
 
-    fileinfomap[filename] = [version, hashlist]
-
-    return True
 
 # PROJECT 3 APIs below
 
@@ -81,10 +97,11 @@ def updatefile(filename, version, hashlist):
 def isLeader():
     """Is this metadata store a leader?"""
     print("IsLeader()")
-    if status==2:
+    if status == 2:
         return True
     else:
         return False
+
 
 # "Crashes" this metadata store
 # Until Restore() is called, the server should reply to all RPCs
@@ -96,6 +113,7 @@ def crash():
     is_crashed = True
     print("Crash()")
     return True
+
 
 # "Restores" this metadata store, allowing it to start responding
 # to and sending RPCs to other nodes
@@ -114,20 +132,20 @@ def isCrashed():
     print("IsCrashed()")
     return is_crashed
 
+
 # Requests vote from this server to become the leader
 def requestVote(serverid, term):
-
     """Requests vote to be the leader"""
     global vote_counter
     global current_term
     global status
     try:
-        c = xmlrpc.client.ServerProxy("http://"+serverid)
-        response,external_term = c.surfstore.answerVote(term)
+        c = xmlrpc.client.ServerProxy("http://" + serverid)
+        response, external_term = c.surfstore.answerVote(term)
         print(serverid, response, external_term)
         if response:
             vote_counter += 1
-	# downgrade a candidate node to a follower node
+        # downgrade a candidate node to a follower node
         elif external_term > current_term:
             current_term = external_term
             status = 0
@@ -136,9 +154,11 @@ def requestVote(serverid, term):
 
     return True
 
+
 def answerVote(candidate_term):
     global current_term
     global timer
+    global status
     # reset the election timeout
     timer.reset()
     timer.set_election_timeout()
@@ -151,42 +171,91 @@ def answerVote(candidate_term):
             print("I won't vote")
             return False, current_term
     except:
-        pass 
+        pass
 
-# Updates fileinfomap
-def appendEntries(serverid, term, fileinfomap):
+    # Updates fileinfomap
+
+
+def appendEntries(serverid, term, fileinfomap, i):
     """Updates fileinfomap to match that of the leader"""
-    global staus
+    global status
     global current_term
-    response, external_term = xmlrpc.client.ServerProxy("http://"+serverid).surfstore.answerAppendEntries(term,fileinfomap)
+    entries = log[next_index[i]:]
+    prev_log_index = next_index[i]
+    prev_log_term = log[next_index[i]][0]
 
-    #if response:
-	# do nothing
-    if response == False:
-	# downgrade a leader node to a follower node
+    response, external_term = xmlrpc.client.ServerProxy("http://" + serverid).surfstore.answerAppendEntries(term,
+                                                                                                            fileinfomap,
+                                                                                                            serverid,
+                                                                                                            prev_log_index,
+                                                                                                            prev_log_term,
+                                                                                                            entries,
+                                                                                                            commit_index)
+    if response:
+        for key, value in fileinfomap.items():
+            xmlrpc.client.ServerProxy("http://" + serverid).surfstore.updatefile(key, value[0], value[1])
+
+    if not response:
+        # downgrade a leader node to a follower node
         if current_term < external_term:
             current_term = external_term
             status = 0
 
-	# TODO: If the update not success, what should happen
+    # TODO: If the update not success, what should happen: because of log inconsistency: decrement nextIndex and retry
 
     return True
 
-def answerAppendEntries(leader_term,fileinfomap):
-    if current_term > leader_term:
-        return False, current_term
+
+def answerAppendEntries(leader_term, fileinfomap, serverid, prev_log_index, prev_log_term, entries, leader_commit):
+    global commit_index
+    global log
+
+    if len(entries) != 0:
+        if current_term > leader_term:
+            return False, current_term
+
+        if log[prev_log_index][0] != prev_log_term:
+            return False, current_term
+
+        for i in range(len(entries)):
+            if prev_log_index + i < len(log) and log[prev_log_index + i][0] != entries[i][0]:
+                log[prev_log_index + i:] = list()
+                for j in range(i, len(entries)):
+                    log.append(entries[j])
+
+        if leader_commit > commit_index:
+            commit_index = min(leader_commit, len(log))
+
+        # TODO: updates fileinfomap
+        for key, value in fileinfomap.items():
+            xmlrpc.client.ServerProxy("http://" + serverid).surfstore.updatefile(key, value[0], value[1])
+
+        print(fileinfomap)
 
     # reset the election timeout
     timer.reset()
     timer.set_election_timeout()
 
-    # TODO: updates fileinfomap
 
 
     return True, current_term
 
+
 def tester_getversion(filename):
     return fileinfomap[filename][0]
+
+
+def get_commit_index():
+    global commit_index
+    return commit_index
+
+
+def reset_next_and_match_index(n, commit_index):
+    global match_index
+    global next_index
+    match_index = [0 for _ in range(n)]
+    next_index = [commit_index for _ in range(n)]
+
 
 # Reads the config file and return host, port and store list of other servers
 def readconfig(config, servernum):
@@ -211,50 +280,61 @@ def readconfig(config, servernum):
         else:
             serverlist.append(hostport)
 
-
     return maxnum, host, port
+
 
 def raft():
     global status
     global current_term
     global vote_counter
-    global timer 
+    global timer
+    global is_update_file
     timer = TimeHandler()
     timer.set_election_timeout()
     while True:
-	# if it is a leader, just send heartbeats
-        if status==2:
+        # if it is a leader, just send heartbeats
+        if status == 2:
             timer.set_heartbeat_timeout(777)
+
             if timer.timecount() > timer.timeout:
                 timer.reset()
                 thread_list = []
-                for s in serverlist:
-                    thread_list.append(threading.Thread(target=appendEntries, args = (s, current_term, fileinfomap)))
+                for i, s in enumerate(serverlist):
+                    thread_list.append(threading.Thread(target=appendEntries, args=(s, current_term, fileinfomap, i)))
                     thread_list[-1].start()
                 for t in thread_list:
                     t.join()
+            is_update_file = False
         else:
-	    
-	    # election time out 
+            # election time out
             if timer.timecount() > timer.timeout:
                 timer.reset()
                 timer.set_election_timeout()
-                status = 1 # make it an candidate
+                status = 1  # make it an candidate
                 current_term += 1
-                vote_counter = 1 # vote for its self
-                print("I am "+str(host)+":"+str(port)+",  I am requesting for vote, my current term is: "+ str(current_term))
+                vote_counter = 1  # vote for its self
+                print("I am " + str(host) + ":" + str(port) + ",  I am requesting for vote, my current term is: " + str(
+                    current_term))
                 thread_list = []
+                print(serverlist)
                 for s in serverlist:
-                    thread_list.append(threading.Thread(target=requestVote, args=(s,current_term)))
+                    thread_list.append(threading.Thread(target=requestVote, args=(s, current_term)))
                     thread_list[-1].start()
                 for t in thread_list:
                     t.join()
-		# get majority votes, become leader
+                # get majority votes, become leader
                 print(num_servers, vote_counter)
-                if vote_counter > num_servers/2:
-                    status = 2 # make it a leader
-                    print("A new leader "+str(host)+":"+str(port) + " in term: "+ str(current_term))
-	
+                if vote_counter > num_servers / 2:
+                    status = 2  # make it a leader
+                    print("A new leader " + str(host) + ":" + str(port) + " in term: " + str(current_term))
+                    #
+                    n = len(serverlist)
+                    serverid = str(host) + ":" + str(port)
+                    new_commit_index = xmlrpc.client.ServerProxy(
+                        "http://" + serverid).surfstore.get_commit_index()
+                    xmlrpc.client.ServerProxy(
+                        "http://" + serverid).surfstore.reset_next_and_match_index(n, new_commit_index)
+
 if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser(description="SurfStore server")
@@ -272,42 +352,54 @@ if __name__ == "__main__":
         # maxnum is maximum number of servers
         maxnum, host, port = readconfig(config, servernum)
 
-
         hashmap = dict()
 
         fileinfomap = dict()
 
+        log = list()
+        log.append([1, []])
+        last_applied = 0
+        commit_index = 0
+        # read: 1, write: 2
+
+        # For leader
+        next_index = list()
+        match_index = list()
+
         print("Attempting to start XML-RPC Server...")
         print(host, port)
         server = threadedXMLRPCServer((host, port), requestHandler=RequestHandler)
-	# Init variables
-        num_servers = len(serverlist)+1
-        status = 0 # 0: follwer, 1: candidate, 2: leader
+        # Init variables
+        num_servers = len(serverlist) + 1
+        status = 0  # 0: follwer, 1: candidate, 2: leader
         is_crashed = False
-        current_term = 0
-	
-        server.register_introspection_functions()
-        server.register_function(ping,"surfstore.ping")
-        server.register_function(getblock,"surfstore.getblock")
-        server.register_function(putblock,"surfstore.putblock")
-        server.register_function(hasblocks,"surfstore.hasblocks")
-        server.register_function(getfileinfomap,"surfstore.getfileinfomap")
-        server.register_function(updatefile,"surfstore.updatefile")
-        # Project 3 APIs
-        server.register_function(isLeader,"surfstore.isLeader")
-        server.register_function(crash,"surfstore.crash")
-        server.register_function(restore,"surfstore.restore")
-        server.register_function(isCrashed,"surfstore.isCrashed")
-        server.register_function(requestVote,"surfstore.requestVote")
-        server.register_function(appendEntries,"surfstore.appendEntries")
-        server.register_function(tester_getversion,"surfstore.tester_getversion")
 
-        server.register_function(answerVote,"surfstore.answerVote")
-        server.register_function(answerAppendEntries,"surfstore.answerAppendEntries")
+        current_term = 0
+
+        server.register_introspection_functions()
+        server.register_function(ping, "surfstore.ping")
+        server.register_function(getblock, "surfstore.getblock")
+        server.register_function(putblock, "surfstore.putblock")
+        server.register_function(hasblocks, "surfstore.hasblocks")
+        server.register_function(getfileinfomap, "surfstore.getfileinfomap")
+        server.register_function(updatefile, "surfstore.updatefile")
+        # Project 3 APIs
+        server.register_function(isLeader, "surfstore.isLeader")
+        server.register_function(crash, "surfstore.crash")
+        server.register_function(restore, "surfstore.restore")
+        server.register_function(isCrashed, "surfstore.isCrashed")
+        server.register_function(requestVote, "surfstore.requestVote")
+        server.register_function(appendEntries, "surfstore.appendEntries")
+        server.register_function(tester_getversion, "surfstore.tester_getversion")
+
+        server.register_function(reset_next_and_match_index, "surfstore.reset_next_and_match_index")
+        server.register_function(get_commit_index, "surfstore.get_commit_index")
+        server.register_function(answerVote, "surfstore.answerVote")
+        server.register_function(answerAppendEntries, "surfstore.answerAppendEntries")
         print("Started successfully.")
         print("Accepting requests. (Halt program to stop.)")
-	
-        t = threading.Thread(target = raft, )
+
+        t = threading.Thread(target=raft, )
         t.start()
 
         server.serve_forever()
